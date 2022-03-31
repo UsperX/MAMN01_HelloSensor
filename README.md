16b55a7f0b478068aa1fc372351c9f842314b33a
UsperX <usperx@hotmail.com>
2022-03-31 16:27:02 +0200

    Add initial compass functionality

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 Assignment in the course MAMN01 - Advanced Interaction Design
 
 # Used guides
+
+## For initial setup:
 - [Android Developer: "Build your first app"](https://developer.android.com/training/basics/firstapp/index.html)
+
+## For implementing compass:
+- [Sensor Tutorial for Android: Getting Started](https://www.raywenderlich.com/10838302-sensors-tutorial-for-android-getting-started)
+- [Simple compass code with Android Studio] (https://www.codespeedy.com/simple-compass-code-with-android-studio/)
+- [How to Create a Compass in Android Code – Tutorial and Source Code] (https://www.androidcode.ninja/android-compass-code-example/)
+- [How to create your own Compass Android App using Magnetic Field and Accelerometer Sensors in Android Studio? – Complete Source code] (https://programmerworld.co/android/how-to-create-your-own-compass-android-app-using-magnetic-field-and-accelerometer-sensors-in-android-studio-complete-source-code/)
--- app/src/main/java/com/example/mamn01_hellosensor/CompassActivity.java
+++ app/src/main/java/com/example/mamn01_hellosensor/CompassActivity.java
@@ -2,23 +2,122 @@ package com.example.mamn01_hellosensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CompassActivity extends AppCompatActivity {
+
+    //Device sensor manager
+    private SensorManager sensorManager;
+    private Sensor sensorAccelerometer;
+    private Sensor sensorMagneticField;
+
+    // Define the compass picture that will be used
+    private ImageView compassNeedle;
+    private TextView degreeDisplay;
+
+    // Record the angle turned of the compass picture;
+    private float currentDegrees = 0f;
+
+    private float[] floatGravity = new float[3];
+    private float[] floatGeoMagnetic = new float[3];
+
+    private float[] floatOrientation = new float[3];
+    private float[] floatRotationMatrix = new float[9];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
-        // Get the Intent that started this activity and extract the string
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        compassNeedle = (ImageView) findViewById(R.id.imageView);
 
-        // Capture the layout's TextView amd set the string as its text
-//        TextView textView = findViewById(R.id.textView);
-//        textView.setText(message);
+        degreeDisplay = (TextView) findViewById(R.id.textView);
+
+        // Initialize android device sensor capabilities
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(sensorEventListenerAccelrometer, sensorAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(sensorEventListenerMagneticField, sensorMagneticField, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Stop listener to save battery
+        sensorManager.unregisterListener(sensorEventListenerAccelrometer);
+        sensorManager.unregisterListener(sensorEventListenerMagneticField);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListenerAccelrometer, sensorAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(sensorEventListenerMagneticField, sensorMagneticField, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    SensorEventListener sensorEventListenerAccelrometer = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            floatGravity = sensorEvent.values;
+
+            SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+            SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+            currentDegrees = (float) (((floatOrientation[0]*180/Math.PI) + 360) % 360);
+            compassNeedle.setRotation(-currentDegrees);
+            degreeDisplay.setText("Heading: " + getDirection(currentDegrees) + " " + (int) (currentDegrees) + " °");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
+
+    SensorEventListener sensorEventListenerMagneticField = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            floatGeoMagnetic = sensorEvent.values;
+
+            SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+            SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+            currentDegrees = (float) (((floatOrientation[0]*180/Math.PI) + 360) % 360);
+            compassNeedle.setRotation(-currentDegrees);
+            degreeDisplay.setText("Heading: " + getDirection(currentDegrees) + " " + (int) (currentDegrees) + " °");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
+
+    private String getDirection(float degrees) {
+        if (degrees >= 350 || degrees <= 10)
+            return "N";
+        else if (degrees < 350 && degrees > 280)
+            return "NW";
+        else if (degrees <= 280 && degrees > 260)
+            return "W";
+        else if (degrees <= 260 && degrees > 190)
+            return "SW";
+        else  if (degrees <= 190 && degrees > 170)
+            return "S";
+        else if (degrees <=170 && degrees > 100)
+            return "SE";
+        else if (degrees <= 100 && degrees > 80)
+            return "E";
+        else return "NE";
     }
 }
